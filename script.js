8ab73d0e522b6efd584be770996e4d8da3406b90
The Hanifah <hanifah2015@gmail.com>
2024-09-04 18:22:59 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+document.getElementById('copy-icon').addEventListener('click', function() {
+    const email = document.getElementById('email').textContent;
+    
+   
+    const tempInput = document.createElement('input');
+    tempInput.value = email;
+    document.body.appendChild(tempInput);
+
+  
+    tempInput.select();
+    document.execCommand('copy');
+    
+    
+    document.body.removeChild(tempInput);
+    
+   
+    this.style.color = 'green';
+    setTimeout(() => {
+        this.style.color = '';
+    }, 1000);
+});
+
+
+document.getElementById('btn-CTA').addEventListener('click', function(){
+    const email = 'hanifahali2015@yahoo.com';
+    const subject = 'Front-end Engineer Interview';
+    const body = 'Hello Hanifah';
+
+   
+    const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+
+    window.location.href = mailtoLink;
+});
+
+
+document.getElementById('resume').addEventListener('click', function() {
+    window.open('https://drive.google.com/file/d/1ng2tiliPXmOWCwNk21IMw5eciykWIIrL/view?usp=sharing');
+});
+
